2eb61e731815237e39d0862f81f93cfdfc153059
Hussein Omar <husseinomar6190@gmail.com>
2022-06-07 20:55:44 +0300

    creating a task model to keep track of all the todos

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,10 @@
+from venv import create
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+   body = models.CharField(max_length=500)
+   done = models.BooleanField(default=False)
+   created = models.DateTimeField(auto_now_add=True)
