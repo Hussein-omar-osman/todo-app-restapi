85e543e414fe5365f4f6f53ff69f44a76337d8a3
Hussein Omar <husseinomar6190@gmail.com>
2022-06-08 11:32:46 +0300

    adding __str__ method to the task model

--- todo_app/models.py
+++ todo_app/models.py
@@ -8,3 +8,6 @@ class Task(models.Model):
    body = models.CharField(max_length=500)
    done = models.BooleanField(default=False)
    created = models.DateTimeField(auto_now_add=True)
+   
+   def __str__(self):
+       return self.body
